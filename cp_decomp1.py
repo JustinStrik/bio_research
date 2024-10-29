4c6b580babcf76d646511904627d4f16d3e283a1
JustinStrik <strikowski.justin@gmail.com>
2024-10-29 11:24:16 -0400

    take input and check error. appears functional

--- cp_decomp1.py
+++ cp_decomp1.py
@@ -1,38 +1,52 @@
 import numpy as np
 import tensorly as tl
 from tensorly.decomposition import parafac
+from read_adj_matrix_input import read_adj_matrix_input
 
 # Initialize the dim-d array arr with random binary values 0 and 1
 # dimensions m, n, o
 
-dimension = 3
-rank = 15
+# dimension = 3
+rank = 2
+
+A = read_adj_matrix_input("results_adj_COHP_44940_480__F_B_w0.csv")
+dimensions = A.shape
 
 # rand value of shape for each dimension
-shape = [np.random.randint(2, 10) for i in range(dimension)]
+# shape = [np.random.randint(2, 10) for i in range(dimension)]
 
-A = np.random.randint(2, size=shape)
+# for testing purposes
+# def init_test_data():
+#     # Initialize the 3d array arr with random binary values 0 and 1
+#     arr = np.random.randint(2, size=shape)
+#     return arr
 
-# Convert A to a floating-point tensor
-A = tl.tensor(A, dtype=np.float64)
+# A = init_test_data()
+# A = np.random.randint(2, size=shape)
+
+# # Convert A to a floating-point tensor
+# A = tl.tensor(A, dtype=np.float64)
 
 # Perform matrix factorization on matrix A with different initialization
-decomposed = parafac(A, rank=rank, n_iter_max=1000, init='random')
+decomposed = parafac(A, rank=rank, n_iter_max=1000, init='svd') # if random, risk of being singular
 
 # test the decomposition on random data
 print( tl.norm(A - tl.kruskal_to_tensor(decomposed)))
 
 # and for specific values in the array
 print("The error for the specific values in the array is: ")
-print(tl.norm(A[1, 2, 3] - tl.kruskal_to_tensor(decomposed)[1, 2, 3]))
+print(tl.norm(A[1, 2] - tl.kruskal_to_tensor(decomposed)[1, 2]))
 
 # find how well the decomposition works on random specific values
 for i in range(100):
     # random values for the indices
-    indices = [np.random.randint(0, shape[j]) for j in range(dimension)]
-    print("index: ", indices, "value: ", A[indices])
-    print("decomposed value: ", tl.kruskal_to_tensor(decomposed)[indices])
+    i = np.random.randint(dimensions[0])
+    j = np.random.randint(dimensions[1])
 
-    print("The error for the specific values in the array is: ")
-    print(tl.norm(A[indices] - tl.kruskal_to_tensor(decomposed)[indices ]))
+    # print the error for the specific values in the array
+    print("The error for", i, j, "is: ")
+    print(tl.norm(A[i, j] - tl.kruskal_to_tensor(decomposed)[i, j]))
+    print("The actual value is: ")
+    print(A[i, j])
+
 
