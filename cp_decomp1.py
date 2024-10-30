06328a66337d74e5751d2e41f4c74c6c3b152444
JustinStrik <strikowski.justin@gmail.com>
2024-10-29 22:17:58 -0400

    no longer adj matrix

--- cp_decomp1.py
+++ cp_decomp1.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorly as tl
 from tensorly.decomposition import parafac, non_negative_parafac
 from read_adj_matrix_input import read_adj_matrix_input, test_mod
-from get_adj_matrix_files import get_adj_matrix_files
+from get_adj_matrix_files import get_adj_matrix_files, get_edge_list_files
 import sys
 
 # constants
@@ -34,39 +34,14 @@ def test_single_value_accuracy(A, decomposed):
 
 if __name__ == "__main__":
 
-    files = get_adj_matrix_files()
+    # files = get_adj_matrix_files()
+    files = get_edge_list_files()
     # A = np.zeros((TOTAL_GENES, TOTAL_GENES * len(TOTAL_MICE), TOTAL_WEEKS))
     # init to have values be -1
-    A = np.full((TOTAL_GENES, TOTAL_GENES * TOTAL_MICE, TOTAL_WEEKS), -1)
-
-    # organize files by mouse id
-    mouse_files = {}
-    for file in files:
-        mouse_id = file.split("_")[4]
-        if mouse_id not in mouse_files:
-            mouse_files[mouse_id] = []
-        mouse_files[mouse_id].append(file)
-
-    for i, mouse_id in enumerate(mouse_files):
-        if i > 1:
-            break
-        matrices = []
-        for file in mouse_files[mouse_id]:
-            read_adj_matrix_input(file, A, i)
-
-    # mouse_matrices = {}
-    # # read the adjacency matrix for each mouse file
-    # # for mouse_id, files in mouse_files.items():
-    # # add iter i to break after 5, test on only 5 mice
-    # for i, (mouse_id, files) in enumerate(mouse_files.items()):
-    #     if i > 1:
-    #         break
-    #     matrices = []
-    #     for file in files:
-    #         matrix_week = file.split("_")[-1].split(".")[0].split('w')[1] # get the week number
-    #         matrices.append((matrix_week, read_adj_matrix_input(file)))
-
-    #     mouse_matrices[mouse_id] = matrices
+    
+    # each sheet is a mouse at a time
+    # each row is an edge connecting two genes
+    val = 0
 
    
     # make A sparse tensor !!!
